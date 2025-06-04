5da506164ab5194cc5ca28d99f0153276fed7a78
afirthes <afirthes@gmail.com>
2025-06-04 13:06:44 +0300

    Initial commit

---
+++ cmd/web/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"github.com/afirthes/ws-quiz/internal/handlers"
+	"log"
+	"net/http"
+)
+
+func main() {
+	mux := routes()
+
+	log.Println("Starting channel listener")
+	go handlers.ListenToWsChannel()
+
+	log.Println("Starting web server on port 8080")
+	_ = http.ListenAndServe(":8080", mux)
+}
---
+++ cmd/web/routes.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"github.com/afirthes/ws-quiz/internal/handlers"
+	"github.com/bmizerany/pat"
+	"net/http"
+)
+
+func routes() http.Handler {
+	mux := pat.New()
+
+	mux.Get("/", http.HandlerFunc(handlers.Home))
+	mux.Get("/ws", http.HandlerFunc(handlers.WsEndpoint))
+
+	fileServer := http.FileServer(http.Dir("./static/"))
+	mux.Get("/static/", http.StripPrefix("/static", fileServer))
+	return mux
+}
